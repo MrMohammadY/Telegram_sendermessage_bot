7843d45aea34ed30aafd27440bbad5033a528c7c
MrMohammadY <m.yazdani000@gmail.com>
2020-04-08 17:37:53 +0430

    end of project and all things is fixed

--- sender_message.py
+++ sender_message.py
@@ -9,8 +9,25 @@ with app:
 
 @app.on_message()
 def my_function(client, message):
-    print(message)
-    app.forward_messages(422188879, message_ids=message.message_id, from_chat_id=message.from_user.id)
+    if message.from_user.id != 422188879:
+        print(message)
+        app.forward_messages(422188879, message_ids=message.message_id, from_chat_id=message.from_user.id)
+
+    if message.from_user.id == 422188879:
+        if message.text:
+            app.send_message(message.reply_to_message.forward_from.id, message.text)
+        elif message.photo:
+            app.send_photo(message.reply_to_message.forward_from.id, message.photo.file_id, caption=message.caption if message.caption else '')
+        elif message.video:
+            app.send_video(message.reply_to_message.forward_from.id, message.video.file_id, caption=message.caption if message.caption else '')
+        elif message.voice:
+            app.send_voice(message.reply_to_message.forward_from.id, message.voice.file_id, caption=message.caption if message.caption else '')
+        elif message.sticker:
+            app.send_sticker(message.reply_to_message.forward_from.id, message.sticker.file_id,)
+        elif message.location:
+            app.send_location(message.reply_to_message.forward_from.id, longitude=message.location.longitude, latitude=message.location.latitude)
+        elif message.animation:
+            app.send_animation(message.reply_to_message.forward_from.id, message.animation.file_id)
 
 
 app.run()
